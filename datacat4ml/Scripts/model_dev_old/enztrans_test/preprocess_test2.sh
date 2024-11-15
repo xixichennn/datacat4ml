e0cc05f7144e8188ae3fe832d6d4f474db32e55b
xixichennn <xixichennn@gmail.com>
2024-11-15 12:31:53 +0100

    rename as 'model_dev_old' when exploring transfermer inplementation

---
+++ datacat4ml/Scripts/model_dev_old/enztrans_test/preprocess_test2.sh
@@ -0,0 +1,110 @@
+#!/bin/bash -l
+#SBATCH --job-name="enztrans_test_preprocess"
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:rtx4090:1
+#SBATCH --ntasks=1
+#SBATCH --mem=28G
+#SBATCH --time=1-00:00:00
+
+conda activate enztrans_test
+workdir=/storage/homefs/yc24j783/datacat4ml/datacat4ml/Scripts/model_dev/enztrans_test
+opennmt_py_dir=/storage/homefs/yc24j783/OpenNMT-py
+
+dataset_dir=${workdir}/data/ki_mor_1_test2
+
+# if the output directory does exist, delete it
+if [ -d ${dataset_dir}/preprocessed_data ]; then
+    rm -r ${dataset_dir}/preprocessed_data
+fi
+
+if [ -d ${dataset_dir}/model ]; then
+    rm -r ${dataset_dir}/model
+fi
+
+if [ -d ${dataset_dir}/Tensorboard ]; then
+    rm -r ${dataset_dir}/Tensorboard
+fi
+
+
+# create the output directory
+mkdir ${dataset_dir}/preprocessed_data
+mkdir ${dataset_dir}/model
+mkdir ${dataset_dir}/Tensorboard
+cd $opennmt_py_dir
+
+
+python preprocess.py \
+        --train_src ${dataset_dir}/train_src.txt \
+        --train_tgt ${dataset_dir}/train_tgt.txt \
+        --valid_src ${dataset_dir}/valid_src.txt \
+        --valid_tgt ${dataset_dir}/valid_tgt.txt \
+        --save_data ${dataset_dir}/preprocessed_data \
+        --src_seq_length 3000 \
+        --tgt_seq_length 3000 \
+        --src_vocab_size 3000 \
+        --tgt_vocab_size 3000 \
+        --share_vocab \
+        --lower
+
+WEIGHT1=1
+WEIGHT2=9
+batchsize=1024
+dropout=0.1
+rnnsize=384
+wordvecsize=384
+learnrate=2
+layers=4
+heads=8
+
+train_steps=100
+
+python train.py \
+        -data ${dataset_dir}/preprocessed_data \
+        -save_model ${dataset_dir}/model \
+        -seed 42 \
+        -save_checkpoint_steps 5000 \
+        -keep_checkpoint 20 \
+        -train_steps ${train_steps} \
+        -param_init 0 \
+        -param_init_glorot \
+        -max_generator_batches 32 \
+        -batch_size ${batchsize} \
+        -batch_type tokens \
+        -normalization tokens \
+        -max_grad_norm 0 \
+        -accum_count 4 \
+        -optim adam \
+        -adam_beta1 0.9 \
+        -adam_beta2 0.998 \
+        -decay_method noam \
+        -warmup_steps 8000 \
+        -learning_rate ${learnrate} \
+        -label_smoothing 0.0 \
+        -layers 4 \
+        -rnn_size ${rnnsize} \
+        -word_vec_size ${wordvecsize} \
+        -encoder_type transformer \
+        -decoder_type transformer \
+        -dropout ${dropout} \
+        -position_encoding \
+        -global_attention general \
+        -global_attention_function softmax \
+        -self_attn_type scaled-dot \
+        -heads 8 \
+        -transformer_ff 2048 \
+        -valid_steps 5000 \
+        -valid_batch_size 4 \
+        -report_every 1000 \
+        -log_file ${dataset_dir}/model/training_log.txt \
+        -early_stopping 10 \
+        -early_stopping_criteria accuracy \
+        -world_size 1 \
+        -gpu_ranks 0 \
+        -tensorboard \
+        -tensorboard_log_dir ${dataset_dir}/Tensorboard
+
+# remove two lines below from the above script
+#-data_ids ENZR ST_sep_aug \
+#-data_weights WEIGHT1 WEIGHT2 \
+
+conda deactivate
