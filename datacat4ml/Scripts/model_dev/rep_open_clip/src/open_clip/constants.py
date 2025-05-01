88d499bd839cb31e9f794c3236a7c66a0d5fb44e
xixichennn <xixichennn@gmail.com>
2025-05-01 09:32:29 +0200

    rename the folder where stores the previous code for model_dev

--- datacat4ml/Scripts/model_dev/rep_open_clip/src/open_clip/constants.py
+++
@@ -1,4 +0,0 @@
-# Default name for a weights file hosted on the Huggingface Hub.
-HF_WEIGHTS_NAME = "open_clip_pytorch_model.bin"  # default pytorch pkl
-HF_SAFE_WEIGHTS_NAME = "open_clip_model.safetensors"  # safetensors version
-HF_CONFIG_NAME = 'open_clip_config.json'
